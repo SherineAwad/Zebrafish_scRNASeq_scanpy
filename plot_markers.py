5ce585c27d31d378ba3df1a744cdeeb943abe951
Sherine Awad <sherinem@gl3132.arc-ts.umich.edu>
2025-02-20 11:50:00 -0500

    update subset, plot, and plot_markers

---
+++ plot_markers.py
@@ -0,0 +1,43 @@
+import scanpy as sc
+import sys
+import importlib_metadata
+import matplotlib.pyplot as plt
+import argparse
+
+sys.modules['importlib.metadata'] = importlib_metadata
+
+parser = argparse.ArgumentParser()
+parser.add_argument('myObject')
+parser.add_argument('markers') 
+args = parser.parse_args()
+
+myObject =  args.myObject
+markers = args.markers
+print(markers)
+
+parts = myObject.split("_")
+plot_name = parts[0] + ".png"
+
+
+with open(markers, 'r') as file:
+    marker_genes = [line.strip() for line in file.readlines()]
+
+print(marker_genes)
+
+combined_adata = sc.read_h5ad(myObject, backed="r")
+
+
+sc.pl.dotplot(combined_adata, 
+              var_names=marker_genes, 
+              groupby='leiden',  
+              save=plot_name) 
+
+sc.pl.violin(
+    combined_adata, 
+    keys=marker_genes,
+    groupby='leiden',   
+    save=plot_name
+)
+
+
+
--- subset.py
+++ subset.py
@@ -21,12 +21,19 @@ myObject =  args.myObject
 parts = myObject.split("_")  
 
 celltype = args.celltype 
+subset_name = celltype +"_"+ parts[1] 
 
 combined_adata = sc.read_h5ad(myObject, backed="r")
 
 subset = combined_adata[combined_adata.obs['celltype'].isin([celltype]), :]
 
-subset_name = celltype +"_"+ parts[1] 
+subset = subset.to_memory()
+
+sc.pp.neighbors(subset)
+sc.tl.leiden(subset)
+
+plot_name = celltype+".png" 
+sc.pl.umap(subset, color='leiden', save=plot_name)
 
 subset.write(subset_name, compression="gzip")
 
