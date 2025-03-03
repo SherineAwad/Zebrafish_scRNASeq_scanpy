0d70e48df9d0fc7ad09de929eff7dca1bed28c3d
Sherine Awad <sherinem@gl3157.arc-ts.umich.edu>
2025-03-03 12:29:24 -0500

    update annotate.py and config to add annotations file

--- annotate.py
+++ annotate.py
@@ -13,74 +13,35 @@ sys.modules['importlib.metadata'] = importlib_metadata
 
 parser = argparse.ArgumentParser()
 parser.add_argument('myObject')
+parser.add_argument('annotations')
 args = parser.parse_args()
 
 myObject =  args.myObject
-parts = myObject.split("_")  
+annot_file = args.annotations
+
+parts = myObject.split("_")
 newObject = "annotated_" + parts[1]
 
+fname = parts[1].split(".")
+sample = fname[0]
 combined_adata = sc.read_h5ad(myObject, backed="r")
 
-combined_adata.obs["celltype"] = combined_adata.obs["leiden"].map(
-    {
 
-        "0" : "MG", 
-        "4" : "MG",
-        "25": "MG",
-    
-        "15": "Progenitors", 
+cluster_to_celltype_dict = {}
+with open(annot_file, "r") as f:
+    for line in f:
+        cluster, celltype = line.strip().split()
+        cluster_to_celltype_dict[cluster] = celltype
 
-        "8": "Post Mitotic Rod Precursors",
+cluster_to_celltype_dict = {str(key): value for key, value in cluster_to_celltype_dict.items()}
 
-        "17": "Rod", 
-        "20": "Rod", 
-        
-        "1": "Cones", 
-        "5": "Cones", 
-        "18": "Cones", 
-
-        "2" : "BC", 
-        "3" : "BC",
-        "6": "BC",
-        "9": "BC",
-        "12": "BC",
-        "13": "BC",
-        "16": "BC",
-        "21": "BC",
-        "22": "BC",
-        "23": "BC",
-        "26": "BC",
-        "30": "BC",
-        "32": "BC",
-        "34": "BC",
-        "35": "BC",
-        "38": "BC",
-        
-        "7": "AC", 
-        "24":"AC",
-        "27":"AC",
-        "33":"AC",
-        "37":"AC",
-        "39":"AC",
-        "42":"AC",
-
-        "14": "RGC", 
-        "29": "Microglia", 
-
-        "41": "Endothelial Cells", 
-
-        "28": "RPE", 
-        "40": "RPE", 
-     
-        "11": "HC", 
-        "19": "HC", 
-        "10": "HC", 
-        "31": "HC" 
-
-    }
-)
+combined_adata.obs["celltype"] = combined_adata.obs["leiden"].map(cluster_to_celltype_dict)
 
 
+figure_name = sample +"_annotationsON.png"
 combined_adata.obs_names_make_unique()
-sc.pl.umap(combined_adata, color='celltype',legend_loc="on data", save="_annotations.png")
+sc.pl.umap(combined_adata, color='celltype',legend_loc="on data", save=figure_name)
+figure_name = sample +"_annotations.png"
+sc.pl.umap(combined_adata, color='celltype',save=figure_name)
+
 combined_adata.write(newObject, compression="gzip")
