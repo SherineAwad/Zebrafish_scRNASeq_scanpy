eb6fc32540317d1c6a1f58b472ad392685537f23
Sherine Awad <sherinem@gl3351.arc-ts.umich.edu>
2025-04-02 14:44:45 -0400

    add findClusters for unannotated clusters

--- findCluster.py
+++ findCluster.py
@@ -6,7 +6,6 @@ combined_adata = sc.read_h5ad("annotated_Zebrafishes.h5ad")
 
 combined_adata.uns['log1p'] = {'base': 1.0}  # Assuming log1p with base 1 (natural log)
 
-'''
 sc.tl.rank_genes_groups(combined_adata, groupby='leiden', groups=['53'], reference='rest', method='wilcoxon')
 cluster_53_results = copy.deepcopy(combined_adata.uns['rank_genes_groups'])
 
@@ -17,7 +16,6 @@ top_genes_53 = pd.DataFrame({
 })
 
 print(top_genes_53.head(10))
-'''
 
 sc.tl.rank_genes_groups(combined_adata, groupby='leiden', groups=['55'], reference='rest', method='wilcoxon')
 cluster_55_results = copy.deepcopy(combined_adata.uns['rank_genes_groups'])
